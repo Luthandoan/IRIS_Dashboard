1a9e62e118feff2289046aab259c860c5db39c4c
Luthando Ndaba <129880881+Luthandoan@users.noreply.github.com>
2025-10-06 15:38:31 +0200

    Delete model_session.py

--- model_session.py
+++
@@ -1,7 +0,0 @@
-class SessionModel:
-    def __init__(self):
-        self.current_session_id = 1
-
-    def save_session(self, duration_seconds):
-        print(f"[SessionModel] Session {self.current_session_id} saved: {duration_seconds:.1f} sec")
-        self.current_session_id += 1
